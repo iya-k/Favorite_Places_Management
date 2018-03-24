3f9fff389df0886c9f23b94a44f8be7dd880f113
KabaSaran <skaba1606@gmail.com>
2018-03-24 15:24:31 +0100

    suppression des dossiers inutile

--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/dao/Map.java
+++
@@ -1,6 +0,0 @@
-package com.example.jetty_jersey.dao;
-
-
-public class Map {
-  int id_map;
-}
--- jetty-jersey-master/src/main/java/com/example/jetty_jersey/dao/Place.java
+++
@@ -1,9 +0,0 @@
-package com.example.jetty_jersey.dao;
-
-
-
-public class Place {
-  int id_place;
-  String location;
-  String associatedMessages;
-}
